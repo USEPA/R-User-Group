7aad888329180fb29e64409c087b4e75562b60bf
unknown <JBEAULIE@LZ2626CJBEAULIE.aa.ad.epa.gov>
2016-02-23 09:29:20 -0500

    Updated data and script for Neptune

--- contributedCode/neptuneFigure/plottingScript.R
+++ contributedCode/neptuneFigure/plottingScript.R
@@ -2,14 +2,15 @@
 library(ggplot2)
 
 # LOAD DATA
-load("contributedCode/neptuneFigure/methaneFigure.RData")  # object is loaded as 'poo'.  Contains emission rate data.
-emis.final <- poo  # 'poo' is silly, give new name
+#ch4RateBest and ch4RateBestError is the methane ebullion rate and ebullition rate error, respectively
+#ch4Diff and ch4Diff95CI is the methane diffusion rate and diffusion rate error, respectively
+load("contributedCode/neptuneFigure/methaneFigure.RData")  # object is loaded as 'methaneFigure'.  Contains emission rate data.
+emis.final <- methaneFigure  # give new name
 
-load("contributedCode/neptuneFigure/poolElevationFigure.RData") # object also loaded as 'poo'.  Contains pool elevation data.
+
+load("contributedCode/neptuneFigure/poolElevationFigure.RData") # object loaded as 'poo'.  Contains pool elevation data.
 all.levels <- poo  # 'poo' is silly, give new name.
 
-# LETS FABRICATE AN CH4 EMISSION RATE ERROR ESTIMATE.  WILL CALCULATE TRUE ERROR ESTIMATE LATER.
-emis.final$error  <- emis.final$ch4RateBest * 0.05  # Assume 5% error
 
 
 # PLOT
@@ -17,8 +18,17 @@ emis.final$error  <- emis.final$ch4RateBest * 0.05  # Assume 5% error
 # Best data: mg ch4/m2/day
 ggplot(emis.final,
   aes(date.time.deployment + ((date.time.logger.end - date.time.deployment)/2), # center of observation period
-      ch4RateBest)) +  # methane emission rate
-  facet_wrap(~fsite, scales = c("free_y")) +  # wrap by ordered factor
+      ch4RateBest)) +  # methane ebullition rate
+  geom_point() +
+  geom_errorbar(aes(ymax = ch4RateBest + ch4RateBestError,  # add ebullition error bars
+                    ymin = ch4RateBest - ch4RateBestError)) +
+  geom_point(aes(date.time.deployment + ((date.time.logger.end - date.time.deployment)/2), ch4Diff), # methane diffusion
+             color = "red") +
+  geom_errorbar(aes(ymax = ch4Diff + ch4Diff95CI, ymin = ch4Diff - ch4Diff95CI), color = "red") + # diffusion error bars
+  geom_segment(aes(x = date.time.deployment, # segment denotes observation period
+                   xend = date.time.logger.end,
+                   y = ch4RateBest,
+                   yend = ch4RateBest)) +  
   geom_rect(  # rectangle to highlight period of special interest.
     xmin=as.numeric(as.POSIXct("2015-09-14 09:00", tz = "UTC")),  # note tz specification
     xmax=as.numeric(as.POSIXct("2015-09-15 09:00", tz = "UTC")),  # note tz specification
@@ -26,14 +36,8 @@ ggplot(emis.final,
     ymax=10000,  # fill entire y-axis range
     fill = "black",
     alpha = 1/50) +  # semi transparent.
-  geom_point() +
-  geom_errorbar(aes(ymax = ch4RateBest + error,  # add error bars
-                ymin = ch4RateBest - error)) +
-  geom_segment(aes(x = date.time.deployment, # segment denotes observation period
-                   xend = date.time.logger.end,
-                   y = ch4RateBest,
-                   yend = ch4RateBest)) + 
   ylab(expression(Methane~Ebullition~(mg~ CH[4]~ m^{-2}~ day^{-1}))) +
+  facet_wrap(~fsite, scales = c("free_y")) +  # wrap by ordered factor
   theme_bw() + 
   theme(axis.title.x = element_blank(),
         panel.grid.minor=element_blank(),
@@ -42,6 +46,12 @@ ggplot(emis.final,
 ggsave(filename="contributedCode/neptuneFigure/bestCh4EbullitionRate.tiff",
        width=10, height=7, units="in",
        dpi=1200,compression="lzw")
+
+# I wonder if the diffusive and ebullitive emission rates should be in the same column,
+# with a new column added for 'emission.type'.  Then we could call 
+# geom_point(aes(color = emission.type)).  I think this approach would also give us
+# a legend.
+
 
 
 # Plot of pool elevations
