ef8d3d7c9ac465b8eba5de967b5ccc2cd198ce7f
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-03-27 11:11:16 -0400

    updated build_list to overwrite index

--- contributedCode/build_list.sh
+++ contributedCode/build_list.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-rm list.html
-echo "<h1> US EPA Shiny Apps </h1>" > list.html
-echo "<ul>" >> list.html
+rm index.html
+echo "<html>"
+echo "<body>"
+echo "<h1> US EPA Shiny Apps </h1>" > index.html
+echo "<ul>" >> index.html
 for d in */ ; do
 echo "<li><a href='http://edap-enviro1.rtpnc.epa.gov:3838/$d)'>$d</a></li>" >> list.html
 done
-echo "</ul>"
-
+echo "</ul>" >> index.html
+echo "</body>"
+echo "</html>"
