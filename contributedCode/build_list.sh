9d5373cdbd27df5c4a8264848bc5a6e8cbfcc7c3
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-03-27 11:27:33 -0400

    might work now...

--- contributedCode/build_list.sh
+++ contributedCode/build_list.sh
@@ -4,9 +4,10 @@ echo "<html>" > index.html
 echo "<body>" >> index.html
 echo "<h1> US EPA Shiny Apps </h1>" >> index.html
 echo "<ul>" >> index.html
-for d in */ ; do
-echo "<li><a href='http://edap-enviro1.rtpnc.epa.gov:3838/$d)'>$d</a></li>" >> list.html
-done
+for d in */ 
+do
+echo "<li><a href='http://edap-enviro1.rtpnc.epa.gov:3838/$d)'>$d</a></li>" 
+done >> list.html
 echo "</ul>" >> index.html
 echo "</body>" >> index.html
 echo "</html>" >> index.html
