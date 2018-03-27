ceb4f6a3ba2fcfd80bda780ae336fd00198bba67
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-03-27 11:17:10 -0400

    fixed build_list.sh

--- contributedCode/build_list.sh
+++ contributedCode/build_list.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 rm index.html
-echo "<html>"
-echo "<body>"
-echo "<h1> US EPA Shiny Apps </h1>" > index.html
+echo "<html>" > index.html
+echo "<body>" >> index.html
+echo "<h1> US EPA Shiny Apps </h1>" >> index.html
 echo "<ul>" >> index.html
 for d in */ ; do
 echo "<li><a href='http://edap-enviro1.rtpnc.epa.gov:3838/$d)'>$d</a></li>" >> list.html
 done
 echo "</ul>" >> index.html
-echo "</body>"
-echo "</html>"
+echo "</body>" >> index.html
+echo "</html>" >> index.html
