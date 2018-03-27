900953f99afec0408c2836b5edd68979ea8f6d69
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-03-27 11:34:46 -0400

    jeff still smart not

--- contributedCode/build_list.sh
+++ contributedCode/build_list.sh
@@ -6,7 +6,7 @@ echo "<h1> US EPA Shiny Apps </h1>" >> index.html
 echo "<ul>" >> index.html
 for d in */ 
 do
-echo "<li><a href='http://edap-enviro1.rtpnc.epa.gov:3838/$d)'>$d</a></li>" 
+echo "<li><a href='http://edap-enviro1.rtpnc.epa.gov:3838/$d'>$d</a></li>" 
 done >> index.html
 echo "</ul>" >> index.html
 echo "</body>" >> index.html
