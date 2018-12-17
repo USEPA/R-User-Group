2db499d4a911fb45d6955f56bece9cf99bf33748
Jeff Hollister <jeff.w.hollister@gmail.com>
2018-12-17 13:52:57 -0500

    blargh!

--- contributedCode/cloud_gov_shiny/rgdal/shiny.R
+++ contributedCode/cloud_gov_shiny/rgdal/shiny.R
@@ -1,9 +1,12 @@
 library(shiny)
 install.packages("praise",repos = "https://cran.r-project.org")
-install.packages("rgdal",repos = "https://cran.r-project.org",
-                 configure.args=c('--with-proj-include=/home/vcap/deps/0/apt/usr/include',
-                                  '--with-proj-lib=/home/vcap/deps/0/apt/usr/lib',
-                                  '--with-gdal-config=/home/vcap/deps/0/apt/usr/bin'))
+#this doesn't appear to be working yet...
+#install.packages("rgdal", type = "source",
+#                 repos = "https://cran.r-project.org",
+#                 configure.args=c('--with-proj-include=/home/vcap/deps/0/apt/usr/include',
+#                                  '--with-proj-lib=/home/vcap/deps/0/apt/usr/lib',
+#                                  '--with-gdal-config=/home/vcap/deps/0/apt/usr/bin'))
+# maybe some info here: https://github.com/cloudfoundry/python-buildpack/issues/25
 runApp(host="0.0.0.0", port=strtoi(Sys.getenv("PORT")))
 
 
